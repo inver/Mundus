bdd30497406dbfb76917ed73ed6e0861494870df
inv3r <anevinsky@gmail.com>
2022-11-17 21:50:54 +0300

    added test for camera serialization

---
+++ commons/src/test/java/com/mbrlabs/mundus/commons/importer/CameraConverterTest.java
@@ -0,0 +1,24 @@
+package com.mbrlabs.mundus.commons.importer;
+
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CameraConverterTest {
+    @Test
+    public void testConversion() {
+        var cam = new PerspectiveCamera();
+        cam.direction.set(1, 1, 1);
+        cam.position.set(2, 2, 2);
+        cam.far = 122;
+        cam.near = 2222;
+
+        var dto = CameraConverter.fromCamera(cam);
+        var newCam = CameraConverter.fromDto(dto);
+
+        Assert.assertEquals(cam.far, newCam.far, 0.0000001);
+        Assert.assertEquals(cam.near, newCam.near, 0.0000001);
+        Assert.assertEquals(cam.position, newCam.position);
+        Assert.assertEquals(cam.direction, newCam.direction);
+    }
+}
